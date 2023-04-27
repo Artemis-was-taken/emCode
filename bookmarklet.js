891fc2cbc3ccc2154ab30683cf721dce0baada3d
Artemis-was-taken <94702957+Artemis-was-taken@users.noreply.github.com>
2023-04-27 10:02:13 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,15 @@
-alert("hello")
+(function() {
+  // Step 1: Ask for URL
+  var url = prompt('Please enter the URL of the target website (e.g. https://www.example.com):');
+
+  // Step 2: Scrape code from target website
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      // Replace the code of the current page with the code from the target website
+      document.documentElement.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error fetching target website:', error);
+    });
+})();
