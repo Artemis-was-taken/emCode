df20c231bf5942ec8d38b55b5f855a81e8de688b
Artemis-was-taken <94702957+Artemis-was-taken@users.noreply.github.com>
2023-04-27 10:23:37 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,15 +1,32 @@
 (function() {
-  // Step 1: Ask for URL
-  var url = prompt('Please enter the URL of the target website (e.g. https://www.example.com):');
-
-  // Step 2: Scrape code from target website
-  fetch(url)
+  // Step 1: Read the links from the links.txt file
+  var links = [];
+  fetch('https://example.com/links.txt')
     .then(response => response.text())
-    .then(html => {
-      // Replace the code of the current page with the code from the target website
-      document.documentElement.innerHTML = html;
+    .then(text => {
+      links = text.split('\n');
+      // Step 2: Create an iFrame for each link
+      var i = 0;
+      function createIFrame() {
+        if (i >= links.length) return;
+        var iframe = document.createElement('iframe');
+        iframe.src = links[i];
+        iframe.style.width = '100%';
+        iframe.style.height = '500px';
+        document.body.appendChild(iframe);
+        // Step 3: Check if the iFrame contains the message "Looks like this page isn't allowed"
+        setTimeout(function() {
+          if (iframe.contentDocument.documentElement.innerText.toLowerCase().includes("looks like this page isn't allowed")) {
+            // Step 4: Move to the next link if the message is detected
+            i++;
+            document.body.removeChild(iframe);
+            createIFrame();
+          }
+        }, 5000);
+      }
+      createIFrame();
     })
     .catch(error => {
-      console.error('Error fetching target website:', error);
+      console.error('Error reading links.txt file:', error);
     });
 })();
